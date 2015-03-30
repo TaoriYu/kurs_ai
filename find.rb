46c43973dd7c6aaeaa5725cf8f5623d9b4f8572a
Taori Yu <taoriyu@gmail.com>
2015-03-31 00:23:09 +0300

    it works

--- find.rb
+++ find.rb
@@ -7,50 +7,56 @@ request = String.new
 
 def responses( request )
 
-	"I'm in responses-method".play
-	puts request
-	
 	case request
 
 		when "yes"
-			'Ok, boss, what do you want?'.play
+			'Ok, tsar, what do you want?'.play
+			request = get_request
 		when "no"
-			'Ok, boss, but let me do something for you'.play
+			'Ok, csar, farewell'.play
+			exit
+		when "how many figures do you see?"
+			how_many
 	end
 end
+
 
 def get_request
 
 	request = ""
 	request = gets.chomp
-	return request
-	
+	responses( request )	
+end
+
+
+def how_many
+
+	original_window = GUI::Window.new "original"
+	hough_window = GUI::Window.new "finding circles"
+
+	image = IplImage::load "./stuff.jpg"
+	gray = image.BGR2GRAY
+
+	result = image.clone
+	original_window.show image
+	detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 50)
+
+	puts number = detect.size
+
+	detect.each do |circle|
+
+		puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
+		result.circle! circle.center, circle.radius, :color => CvColor::Blue, :thickness => 3
+	end
+
+	"There are #{number} circles on this image.".play
+
+	hough_window.show result
+	GUI::wait_key	
 end
 
 "Hello. Can I do something for you?".play
-request = get_request
-responses( request )
-request = get_request
+get_request
 
 
-original_window = GUI::Window.new "original"
-hough_window = GUI::Window.new "finding circles"
 
-image = IplImage::load "./circles.jpg"
-gray = image.BGR2GRAY
-
-result = image.clone
-original_window.show image
-detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 50)
-
-puts number = detect.size
-
-detect.each do |circle|
-	puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
-	result.circle! circle.center, circle.radius, :color => CvColor::Blue, :thickness => 3
-end
-
-"There are #{number} circles on this image.".play
-
-hough_window.show result
-GUI::wait_key
